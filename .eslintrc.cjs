4d2f9c889c907d47167664d928ca1ae608d8fc79
Hilverton Cesar dos Santos Oliveira <hilvertoncesar@gmail.com>
2023-01-24 19:41:49 -0300

    initial configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "standard-with-typescript"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "comma-dangle": "off",
+  },
+};
